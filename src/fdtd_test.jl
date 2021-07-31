3da5a379ed439bb9f1d49e54ff7a4f57464e93c4
asandweech <asandweech@gmail.com>
2021-07-31 01:18:43 -0600

    start adding plots

--- src/fdtd_test.jl
+++ src/fdtd_test.jl
@@ -1,13 +1,19 @@
 
+using Plots
+gr()
+
 include( "SemsFDTD.jl" )
 
-testsim = SemsFDTD.new_sim( Float32, 400, 400, 400 )
+testsim = SemsFDTD.new_sim( Float16, 400, 400, 50 )
 
 SemsFDTD.timestep( testsim )
 
 @time begin
-    for i = 1:10
+    for i = 1:5
         SemsFDTD.timestep( testsim )
         print( ' ', i )
     end
 end
+
+heatmap( testsim.coeff_e_xe[:,:,2], aspect_ratio = 1 )
+
